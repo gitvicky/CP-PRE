39bdc7f5c2068475fa1b706c3eddce1275814194
Vignesh Gopakumar <Vicky@Vigneshs-Macbook.local>
2024-02-28 15:34:57 +0000

    Further exploring stencil learning

--- Stencil/Tests/kernel_learning_stencil.py
+++ Stencil/Tests/kernel_learning_stencil.py
@@ -15,12 +15,14 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from matplotlib import cm 
 import torch 
+import torch.nn as nn
 import torch.nn.functional as F
 from pyDOE import lhs
+from tqdm import tqdm 
 # %% 
 #Generating the datasets: Performing the Laplace Operator over a range of ICs. 
-
-def generate_input_data(N):
+#This will be the training input
+def generate_IC(N):
 
     x = np.linspace(1, -1, 31)
     y = x.copy()
@@ -38,17 +40,116 @@ def generate_input_data(N):
         u_ic.append(np.exp(amp[ii]*((xx-x_pos[ii])**2 + (yy-y_pos[ii])**2)))
     return np.asarray(u_ic)
 
-uu = generate_input_data(10)
+N = 10000
+uu = generate_IC(N)
 uu = torch.Tensor(uu)
 
 # %% 
-#Obtaining the laplacianm of the field evaluated using the 5-point stencil. 
-def generate_output_data(X):    
+#Obtaining the laplacian of the field evaluated using the 5-point stencil. 
+#This will be the training target
+def laplace_stencil(X):    
     laplace_kernel = torch.tensor([[0., 1., 0.],
                            [1., -4., 1.],
                            [0, 1., 0.]])
     
     conv = F.conv2d(X.view(X.shape[0], 1, X.shape[1], X.shape[2]), laplace_kernel.view(1,1,3,3))
+    return conv
 
-uu_laplace = generate_output_data(uu)
+uu_laplace = laplace_stencil(uu)
 
+# %% 
+#Setting up the one layer convolutional network 
+
+# 1D convolutional laplace 
+class conv_laplace(nn.Module):
+    def __init__(self, activation=None):
+        super(conv_laplace, self).__init__()
+
+        #Convolutional Layers
+        self.conv = nn.Conv2d(1, 1, (3,3), stride=1)
+
+    def forward(self, x):
+        x =self.conv(x)
+        return x
+    
+
+learnt_laplace = conv_laplace()
+
+loss_func = torch.nn.MSELoss() #LogitsLoss contains the sigmoid layer - provides numerical stability. 
+optimizer = torch.optim.Adam(learnt_laplace.parameters(), lr=1e-3)
+
+# %% 
+#Prepping the data. 
+X_train = uu.view(uu.shape[0], 1, uu.shape[1], uu.shape[2])
+Y_train = uu_laplace
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_train, Y_train), batch_size=1000, shuffle=True)
+
+
+# %% 
+#Training the Convolutional Network 
+epochs = 1000
+loss_val = []
+for ii in tqdm(range(epochs)):    
+    for xx, yy in train_loader:
+        optimizer.zero_grad()
+        y_out = learnt_laplace(xx)
+        loss = loss_func(y_out, yy)
+        loss.backward()
+        optimizer.step()
+    loss_val.append(loss.item())
+
+torch.save(learnt_laplace, "learnt_laplace.pth")
+plt.plot(np.arange(1, epochs+1), np.asarray(loss_val))
+plt.xlabel("Epochs")
+plt.ylabel("MSE Loss")
+plt.title("Training Loss")
+
+# %% 
+#Learning the inverse of the laplace 
+
+# 1D tranposed convolution for the inverse laplace
+class transp_conv_inv_laplace(nn.Module):
+    def __init__(self, activation=None):
+        super(transp_conv_inv_laplace, self).__init__()
+
+        #Convolutional Layers
+        self.transp_conv = nn.ConvTranspose2d(1, 1, (3,3), stride=1)
+
+    def forward(self, x):
+        x =self.transp_conv(x)
+        return x
+    
+
+inv_laplace = transp_conv_inv_laplace()
+
+loss_func = torch.nn.MSELoss() #LogitsLoss contains the sigmoid layer - provides numerical stability. 
+optimizer = torch.optim.Adam(inv_laplace.parameters(), lr=1e-3)
+
+# %% 
+#Prepping the data. 
+X_train = uu_laplace
+Y_train = uu.view(uu.shape[0], 1, uu.shape[1], uu.shape[2])
+
+train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_train, Y_train), batch_size=1000, shuffle=True)
+
+
+# %% 
+#Training the Convolutional Network 
+epochs = 10000
+loss_val = []
+for ii in tqdm(range(epochs)):    
+    for xx, yy in train_loader:
+        optimizer.zero_grad()
+        y_out = inv_laplace(xx)
+        loss = loss_func(y_out, yy)
+        loss.backward()
+        optimizer.step()
+    loss_val.append(loss.item())
+
+torch.save(learnt_laplace, "inv_laplace.pth")
+plt.plot(np.arange(1, epochs+1), np.asarray(loss_val))
+plt.xlabel("Epochs")
+plt.ylabel("MSE Loss")
+plt.title("Training Loss")
+# %% 
