d1394291607a7869e8ff98941a09856dd3da43cf
Vignesh Gopakumar <Vicky@eduroam-int-dhcp-97-177-126.ucl.ac.uk>
2025-02-10 17:10:04 +0000

    SHO final

--- Inverse_residuals/test/SHO_node_test.py
+++ Inverse_residuals/test/SHO_node_test.py
@@ -413,13 +413,13 @@ residual_pred = res[400:]
 # plt.grid(True)
 
 # %% 
-#Inverse - Position
+#Inverse - Position : To perform the inverse you need the complete information. 
 pos_res = D_pos.differentiate(torch.tensor(neural_sol[...,0], dtype=torch.float32), correlation=False, slice_pad=False)
 pos_retrieved = D_pos.integrate(pos_res, correlation=False, slice_pad=False)
 # vel_retrieved = D_vel.diff_integrate(v)
 
-plt.plot(neural_sol[0, 1:-1, 0], 'b-', label='Actual')
-plt.plot(pos_retrieved[0,2:-2], 'r--', label='Retrieved')
+plt.plot(neural_sol[0, :, 0], 'b-', label='Actual')
+plt.plot(pos_retrieved[0,1:-1], 'r--', label='Retrieved')
 plt.xlabel('Time')
 plt.ylabel('Position')
 plt.title('Inverse')
